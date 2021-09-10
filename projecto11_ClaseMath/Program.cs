ae4e99b03d7460593fc4526423b99a2a849d0632
william <factzure@hotmail.com>
2021-09-09 19:38:01 -0500

    Agregar archivos de proyecto.

---
+++ projecto11_ClaseMath/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projecto11_ClaseMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM1;
+            string linea;
+            long RESUL;
+            Console.Write("DIGITE UN NUMERO :");
+            linea = Console.ReadLine();
+            NUM1 = int.Parse(linea);
+            RESUL = Math.Abs(NUM1);
+            Console.WriteLine("valor absoluto : " + RESUL);
+            Console.WriteLine("Potencia : " + Math.Pow(NUM1, 3));
+            Console.WriteLine("raiz cuadrada : " + Math.Sqrt(NUM1));
+            Console.WriteLine("SENO : " + Math.Sin(NUM1 * Math.PI / 180));
+            Console.WriteLine("Coseno :" + Math.Cos(NUM1 * Math.PI / 180));
+            Console.WriteLine("tangente : " + Math.Tan(NUM1 * Math.PI / 180));
+            Console.WriteLine("Numero maximo : " + Math.Max(NUM1, 50));
+            Console.WriteLine("Numero minimo : " + Math.Min(NUM1, 50));
+            Console.WriteLine("Parte entera : " + Math.Truncate(18.78));
+            Console.WriteLine("Redondeo de numero : " + Math.Round(18.78));
+
+            Console.ReadLine();
+        }
+    }
+}
